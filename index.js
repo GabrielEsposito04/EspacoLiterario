7820059db997238659828076621d8b83af8da926
Gabriel Esposito <gabriel.esposito04@gmail.com>
2024-06-06 22:49:25 -0300

    commit

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-import { handleDropdownToggle } from './autenticacao.js';
-
 const botaoGoogle = document.getElementById('botaoGoogle');
 
-botaoGoogle.onclick = () => {
-    // Verifica se a função handleDropdownToggle está definida
-    if (typeof handleDropdownToggle === 'function') {
-        handleDropdownToggle();
-    } else {
-        // Se não estiver definida (neste contexto, no index), chama a função handleLogin
-        handleLogin();
-    }
-};
