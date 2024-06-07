c0b403b41a6fad6c8c47cbcfcaf60c338152961f
Gabriel Esposito <gabriel.esposito04@gmail.com>
2024-06-07 00:34:13 -0300

    commit

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Defina os cabeçalhos necessários
+
 app.use((req, res, next) => {
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   next();
 });
 
-// Servir arquivos estáticos
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
